2bd304d4a3622a5c7363847e8c5e12e85aef1a03
Brendon Crave <brennycode@gmail.com>
2023-03-24 15:36:03 +0400

    updated seed data

--- foundation_server/team.py
+++ foundation_server/team.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, session
+from foundation_server.db import get_db
 
-bp = Blueprint("team", __name__, "/team")
+bp = Blueprint("teams", __name__, "/teams")
+
+
+@bp.route("", methods=("GET", "POST"))
+def teams():
+    user_id = session.get("user_id")
+
+    if user_id is not None:
+        db = get_db()
